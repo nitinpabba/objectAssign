1ca8caf2a403b5009c4e8f8ee56f21406bc26ed2
Nitin Pabba <nitinpabba@Nitins-MBP.home>
2015-03-17 18:44:31 -0400

    Initial Commit

---
+++ objectAssgn/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objectAssgn
+//
+//  Created by Nitin Pabba on 3/17/15.
+//  Copyright (c) 2015 Nitin Pabba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

bd726105800499134132f9faabc367fe6c6ddc83
Nitin Pabba <nitinpabba@Nitins-MBP.home>
2015-03-17 19:58:02 -0400

    first commit

---
+++ animal.h
@@ -0,0 +1,13 @@
+//
+//  animal.h
+//  objectAssgn
+//
+//  Created by Nitin Pabba on 3/17/15.
+//  Copyright (c) 2015 Nitin Pabba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface animal : NSObject
+
+@end
---
+++ animal.m
@@ -0,0 +1,13 @@
+//
+//  animal.m
+//  objectAssgn
+//
+//  Created by Nitin Pabba on 3/17/15.
+//  Copyright (c) 2015 Nitin Pabba. All rights reserved.
+//
+
+#import "animal.h"
+
+@implementation animal
+
+@end
